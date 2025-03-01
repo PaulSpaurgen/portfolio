78bcdec865144f4d1d17a27e19ed32c344840950
paul <paulspaurgen@gmail.com>
2025-03-01 23:05:54 +0530

    import fix

--- src/components/LoadingWrap/index.js
+++ src/components/LoadingWrap/index.js
@@ -1,6 +1,11 @@
-import Lottie from "react-lottie";
+
 import animationData from "../../../public/animation.json";
 import { useMobile } from "@/context/MobileContext";
+import dynamic from 'next/dynamic';
+
+// Dynamically import Lottie with ssr disabled
+const Lottie = dynamic(() => import('react-lottie'), { ssr: false });
+
 
 export default function LoadingWrap() {
   const { isMobile } = useMobile();
@@ -12,8 +17,6 @@ export default function LoadingWrap() {
   return (
     <div className="fixed top-0 left-0 w-[100vw] h-[100vh] bg-black flex flex-col justify-center items-center z-50">
       <Lottie options={defaultOptions} height={isMobile ? 300 : 400} width={isMobile ? 300 : 600} />
-
-      <p className="text-white text-2xl font-bold mt-4 animate-pulse">Loading</p>
     </div>
   );
 }
